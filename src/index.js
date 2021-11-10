f6d00b1013f78b30eef31d20ba93b76c2dfc3ac9
Dallas Pataska <pataskad@gmail.com>
2021-11-10 14:43:44 -0600

    refactored renderDisplay for modularity, clarity, and ease of reading.  Also created ToDoItem class with necessary properties, no connection to UI yet

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import _ from 'lodash'
 import './styles.css'
 import { displayRender } from './modules/displayRender.js'
+import { ToDoItem, myToDoItems } from './modules/createToDo.js'
  
 
 window.onload = function() {
     // KEEP SOLID PRINCIPLES IN MIND! REFACTOR WHERE NEEDED, START ROUGH AND REFINE
 
+
+    // place array containing todo's in main or module??
+/*     let myToDoItems = [
+        {
+          title: "Placeholder Title",
+          description: "Placeholder Description",
+          dueDate: "5:00pm November 19th, 2021", // use date fns package for modifying/setting date?
+          priority: "Low", // priority possibilities (Low, Med, High)  
+        },
+    ] */
+
     // renders basic layout, nav, header, and formatting.
     displayRender() 
-    
+
+    let test = new ToDoItem("Placeholder Title", "Placeholder Description", "Nov 5th, 5pm", "Low")
+    myToDoItems.push(test)
+    console.log(myToDoItems)
+
     // house everything in window.onload??
 }
